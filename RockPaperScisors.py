5cd545d9ce0686bacfe945b9cf47e2fd1c2ec36a
Alessio <sansiverialessio55@gmail.com>
2026-04-27 21:11:41 +0200

    Basic input and doesn't crash if you put a wrong number

---
+++ RockPaperScisors.py
@@ -0,0 +1,8 @@
+Risposta = int(input("Select:\n1) Rock\n2) Paper\n3) Scisors\n(1;2;3): "))
+if Risposta <4:
+	if Risposta >= 1:
+		print(f"Hai selezionato la risposta : {Risposta}")
+	else:
+		print("Don't cheat")
+else:
+	print("Don't cheat")
